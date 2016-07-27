99bcedc84344374830a55dc6513355023410d1da
mpatterson89 <michaelpatterson@ymail.com>
2016-07-27 17:47:47 -0500

    changes to main

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+ip = ""
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+
+
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  	console.log('RECIEVED!!')
+	res.send('Starbound Server IP: '+ip);
+});
+
+// POST method route
+app.post('/', function (req, res) {
+  console.log('POST! ',req.body )
+  ip = req.body.ip
+  res.send('POST request to the homepage');
 });
 
 app.listen(app.get('port'), function () {
